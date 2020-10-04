da65465cd9c857b873a04beb39b2bbaec20105b9
Idan Stark <idanstark42@gmail.com>
2020-10-04 22:49:13 +0300

    removed old SRD

--- srd/moves/basic_moves/hack_and_slash.js
+++
@@ -1,6 +0,0 @@
-import Move from '../move'
-
-const hackAndSlash = new Move.Triggered('When you attack an enemy in melee',
-	new Move.Triggered.Node.Roll.classic('roll+Str',
-		Move.Triggered.Node.Choice('you may choose to do +1d6 damage but expose yourself to the enemy’s attack', [
-			Move.Triggered.Node.Result('')])))
