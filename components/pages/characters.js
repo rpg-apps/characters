45db91829186592b577d4208e72d56672cb7960d
Shira Negbi <shiran96@gmail.com>
2021-04-23 14:37:38 +0300

    changed background color

--- components/pages/characters.js
+++ components/pages/characters.js
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
   	backgroundColor: '#00ff00'
   },
   character: {
-  	backgroundColor: '#aaffff',
+  	backgroundColor: '#00e0ff',
   	width: '100%',
   	height: '3rem',
   	marginBottom: '1rem',
