d3f6a48b069f8d3f8369d317d486d45b869d74b7
Idan Stark <idanstark42@gmail.com>
2021-04-26 01:35:11 +0300

    Added images for classes and colors to characters page

--- src/components/pages/characters.jsx
+++ src/components/pages/characters.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { paramCase as classCase } from 'change-case'
 
-import '../../css/pages/characters.css'
+import '../../css/pages/characters.scss'
 
 import { getAll } from '../../data/character_list'
 
@@ -20,19 +21,18 @@ export default class Characters extends React.Component {
     if (!this.state.characters) {
       return <div className='loader' />
     }
-
     return <div id='characters'>
       {this.state.characters.map(character =>
-        <Link className={`character ${character.characterClass} ${character.active ? 'active' : ''}`} to={`/character/${character.name}`} key={character.name}>
+        <Link className={`character ${classCase(character.characterClass)} ${character.active ? 'active' : ''}`} to={`/character/${character.name}`} key={character.name}>
           <div className='name'>{character.name}</div>
-          <div className='adventure'>{character.adventure}</div>
-          <div className='race'>{character.race} {character.characterClass}</div>
+          <div className='meta'>Lvl. {character.level} {character.race} {character.characterClass}</div>
+          <div className='meta'>{character.adventure}</div>
         </Link>)}
       <Link className='add-character' to='/new'>
         <svg width="100%" height="100%">
-          <line x1="20%" y1="50%" x2="80%" y2="50%" stroke="darkgray" strokeWidth="3px"></line>
-          <line x1="50%" y1="20%" x2="50%" y2="80%" stroke="darkgray" strokeWidth="3px"></line>
-        </svg>
+          <line x1="10%" y1="50%" x2="90%" y2="50%" stroke="rgba(22,23,67,1)" strokeWidth="5px"></line>
+          <line x1="50%" y1="10%" x2="50%" y2="90%" stroke="rgba(22,23,67,1)" strokeWidth="5px"></line>
+          </svg>
       </Link>
     </div>
   }
