3345455df82a41fd84f4f2f6b13b8317f6ebedb5
Idan Stark <idanstark42@gmail.com>
2022-02-20 14:10:53 +0200

    Added basic array input - untested

--- src/components/presentation/input.jsx
+++ src/components/presentation/input.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 export default function Input ({ text, value, type, onChange }) {
+  if (type.endsWith(' array')) {
+    const itemType = type.substring(0, type.length - ' array'.length)
+    return <ArrayInput {...{ text, value, itemType, onChange }} />
+  }
+
   const InputType = InputTypes[type]
   return <InputType text={text} value={value} onChange={onChange} />
 }
@@ -27,6 +32,34 @@ function LongTextInput ({ text, value, onChange }) {
   return <div className='text input'>
     <label>{text}</label>
     <textarea value={value} onChange={e => onChange(e.target.value)} />
+  </div>
+}
+
+function ArrayInput ({ text, value, itemType, onChange }) {
+  const itemChange = index => (newItem => {
+    value[index] = newItem
+    return onChange(value)
+  })
+
+  const itemDelete = index => () => {
+    value.splice(index, 1)
+    return onChange(value)
+  }
+
+  const itemAdd = () => {
+    // TODO add item generation based on type
+    return onChange(value)
+  }
+
+  return <div className='array input'>
+    <label>{text}</label>
+    <div className='items'>
+      {value.map((item, index) => <div className='item'>
+        <Input value={item}, type={itemType} onChange={itemChange(index)} text=''/>
+        <div className='delete' onClick={itemDelete(index)} />
+      </div>)}
+      <div className='add' onClick={itemAdd} />
+    </div>
   </div>
 }
 
