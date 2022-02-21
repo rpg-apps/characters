9216c92ae7fcc1365df5ef66865e9953332209b0
Idan Stark <idanstark42@gmail.com>
2022-02-21 12:17:02 +0200

    Added babel plugin-proposal-optional-chaining

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,7 @@ const { override, addBabelPlugins  } = require('customize-cra')
 
 module.exports = override(
   ...addBabelPlugins(
-    '@babel/plugin-proposal-logical-assignment-operators'
+    '@babel/plugin-proposal-logical-assignment-operators',
+    '@babel/plugin-proposal-optional-chaining'
   )
 )
