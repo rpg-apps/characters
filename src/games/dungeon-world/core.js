93878502a13126798fdea7d5779b70cdfd39db90
Idan Stark <idanstark42@gmail.com>
2022-02-21 15:20:30 +0200

    using executableMoves setting

--- src/games/dungeon-world/core.js
+++ src/games/dungeon-world/core.js
@@ -112,9 +112,10 @@ const getHandlers = settings => {
   }
   handlers['damage-formula'] = { click: 'deal damage' }
 
-  handlers['moves\\.\\d+\\.name'] = {
-    click: (e, moveName) => `trigger ${moveName}`
+  if (settings.collection.executableMoves) {
+    handlers['moves\\.\\d+\\.name'] = { click: (e, moveName) => `trigger ${moveName}` }
   }
+
 
   // -------------------- stats --------------------
   const stats = { strength: 'weak', dexterity: 'shakey', constitution: 'sick', intelligence: 'stunned', wisdom: 'confused', charisma: 'scarred' }
