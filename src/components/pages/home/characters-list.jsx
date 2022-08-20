00f3f0cd012e0b893ecf4a00f5ae4be98a5c2b3e
Idan Stark <idanstark42@gmail.com>
2022-08-20 03:31:46 +0300

    better encapsulation in home page

---
+++ src/components/pages/home/characters-list.jsx
@@ -0,0 +1,44 @@
+import { Link, useHistory } from 'react-router-dom'
+
+import { useCharacters } from '../../contexts/characters-context'
+import Character from '../../presentation/character'
+
+const HOME_EMPTY_MESSAGE = 'Maybe create a character instead of staring at an empty page?'
+const GRAVEYARD_EMPTY_MESSAGE = 'Isn\'t it a good thing no one died?'
+
+export default function CharactersList ({ graveyard }) {
+  const characters = useCharacters()
+  const history = useHistory()
+
+  const revive = async character => {
+    await character.revive()
+    await character.save()
+    history.push(`/character/${character.id}`)
+  }
+
+  const filteredCharacters = characters.filter(character => character.alive === !graveyard)
+
+  return <div className='characters'>
+    {filteredCharacters.length === 0 ? <Empty graveyard={graveyard} /> : <List graveyard={graveyard} characters={filteredCharacters} revive={revive} />}
+  </div>
+}
+
+function Empty ({ graveyard }) {
+  return <div className='empty characters'>
+    <div className='primary'>No one is here</div>
+    <div>{graveyard ? GRAVEYARD_EMPTY_MESSAGE : HOME_EMPTY_MESSAGE}</div>
+  </div>
+}
+
+function List ({ graveyard, characters, revive }) {
+  const Card = graveyard ? DeadCharacterCard : LiveCharacterCard
+  return characters.map((character, index) => <Card character={character} key={index} revive={revive} />)
+}
+
+function DeadCharacterCard ({ character, revive }) {
+  return <Character character={character} ui='character-card' className='dead' onClick={() => revive(character)}/>
+}
+
+function LiveCharacterCard ({ character }) {
+  return <Character character={character} ui='character-card' Component={Link} to={`/character/${character.id}`}/>
+}
--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,46 +1,22 @@
 import { useState, useEffect } from 'react'
-import { Link, useHistory } from 'react-router-dom'
 
 import '../../../css/pages/home.scss'
 
-import { useCharacters } from '../../contexts/characters-context'
-import Footer from './footer'
 import Header from './header'
-import Character from '../../presentation/character'
-
-const HOME_EMPTY_MESSAGE = 'Maybe create a character instead of staring at an empty page?'
-const GRAVEYARD_EMPTY_MESSAGE = 'Isn\'t it a good thing no one died?'
+import CharactersList from './characters-list'
+import Footer from './footer'
 
 export default function Home (props) {
   const [init, setInit] = useState(true)
   const [graveyard, setGraveyard] = useState(false)
-  const characters = useCharacters()
-  const history = useHistory()
 
   useEffect(() => { setTimeout(() => setInit(false), 0) }, [])
 
   const toggleGraveyard = () => setGraveyard(!graveyard)
 
-  const revive = async character => {
-    await character.revive()
-    await character.save()
-    history.push(`/character/${character.id}`)
-  }
-
-  const filteredCharacters = characters.filter(character => character.alive === !graveyard)
-  const content = filteredCharacters.length ?
-    filteredCharacters.map((character, index) => graveyard ? <Character character={character} ui='character-card' className='dead' onClick={() => revive(character)} key={index} to={`/character/${character.id}`}/> : <Character character={character} ui='character-card' Component={Link} key={index} to={`/character/${character.id}`}/>)
-    :
-    <div className='empty characters'>
-      <div className='primary'>No one is here</div>
-      <div>{graveyard ? GRAVEYARD_EMPTY_MESSAGE : HOME_EMPTY_MESSAGE}</div>
-    </div>
-
   return <div className={`home page ${init ? 'init' : ''}`}>
     <Header graveyardControl={[graveyard, setGraveyard]} />
-    <div className='characters'>
-      {content}
-    </div>
+    <CharactersList graveyard={graveyard} />
     <Footer />
   </div>
 }
